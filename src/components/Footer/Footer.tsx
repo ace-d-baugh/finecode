b7715d01b8f595f232669da58a807b54d3dbfc5e
Ace Baugh <ace.d.baugh@gmail.com>
2025-05-11 14:08:59 -0400

    styles in the right place, foot styled

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,5 +1,6 @@
 // components/Footer/Footer.tsx
 
+import { Link } from 'react-router-dom';
 import Frame from '../Frame/Frame'
 import './Footer.css'
 
@@ -7,6 +8,46 @@ function Footer({ company }: { company: string }) {
     return (
         <footer className="footer">
             <Frame>
+                <ul className="footer-links">
+                    <li>
+						<Link to="/about">About</Link>
+					</li>
+					<li>
+						<Link to="/portfolio">Portfolio</Link>
+					</li>
+					<li>
+						<Link to="/resume">Resum&eacute;</Link>
+					</li>
+					<li>
+						<Link to="/blog">Blog</Link>
+					</li>
+					<li>
+						<Link to="/contact">Contact</Link>
+					</li>
+                </ul>
+                <ul className="footer-links">
+                    <li className="footer-link">
+                        <a href="https://github.com/ace-d-baugh" target="_blank" >
+                            <i className="nf nf-fa-github">
+                                <span className="ml-1">GitHub</span>
+                            </i>
+                        </a>
+                    </li>
+                    <li className="footer-link">
+                        <a href="https://www.linkedin.com/in/ace-d-baugh/" target="_blank">
+                            <i className="nf nf-md-linkedin">
+                                <span className="ml-1">LinkedIn</span>
+                            </i>
+                        </a>
+                    </li>
+                    <li className="footer-link">
+                        <a href="https://x.com/acieffe" target="_blank">
+                            <i className="nf nf-fa-twitter_square">
+                                <span className="icon-name ml-1">Twitter/X</span>
+                            </i>
+                        </a>
+                    </li>
+                </ul>
                 <p>&copy; {new Date().getFullYear()} {company}. All rights reserved.</p>
             </Frame>
         </footer>
