b98832da37d83a6e132344a753179a0003e80f9c
Ace Baugh <ace.d.baugh@gmail.com>
2025-05-16 22:53:36 -0400

    Update ShortBio.tsx

--- src/components/ShortBio/ShortBio.tsx
+++ src/components/ShortBio/ShortBio.tsx
@@ -10,7 +10,7 @@ function ShortBio() {
             <Frame className="all">
                 <div>
                     <h1>ShortBio</h1>
-<p>As a full-stack web developer with a Bachelor's degree in Web Development and certifications in Mobile Application Development and Internet Marketing, I specialize in creating dynamic web and mobile applications. My experience includes working on a high-profile project for the Walt Disney Company. Additionally, my certifications in mobile app development and internet marketing enable me to deliver comprehensive solutions that drive user engagement and business growth. I am passionate about leveraging technology to create impactful applications and am committed to continuous learning and innovation.</p>
+                        <p>As a full-stack web developer with a Bachelor's degree in Web Development and certifications in Mobile Application Development and Internet Marketing, I specialize in creating dynamic web and mobile applications. My experience includes working on a high-profile project for the Walt Disney Company. Additionally, my certifications in mobile app development and internet marketing enable me to deliver comprehensive solutions that drive user engagement and business growth. I am passionate about leveraging technology to create impactful applications and am committed to continuous learning and innovation.</p>
                     <Button text="Get To Know Me" link="/about" />
                 </div>
             </Frame>
