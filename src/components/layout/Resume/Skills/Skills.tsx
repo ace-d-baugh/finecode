64787af78da5952d950e8257e99ad443f68c6792
Ace Baugh <ace.d.baugh@gmail.com>
2025-07-24 12:17:29 -0400

    skills on the way

--- src/components/layout/Resume/Skills/Skills.tsx
+++ src/components/layout/Resume/Skills/Skills.tsx
@@ -4,18 +4,27 @@ import Frame from "../../../common/Frame/Frame"
 import './Skills.css';
 import Data from "../../../../assets/data/skills.json"
 
+const leftArray = Data.filter((_: string, index: number) => index % 2 === 0);
+const rightArray = Data.filter((_: string, index: number) => index % 2 !== 0);
 function Skills() {
 	return (
-		<nav className="Skills">
+		<div className="Skills">
 			<Frame className="all">
                 <h3>Skills</h3>
-				<ul>
-				{ Data.map((value: string) => (
-					<li>{value}</li>
-				)) }
-				</ul>
+				<div className="skills-display">
+					<ul>
+					{leftArray.map((value: string) => (
+						<li className="left">{value}</li>
+					))}
+					</ul>
+					<ul>
+					{rightArray.map((value: string) => (
+						<li className="right">{value}</li>
+					))}
+					</ul>			
+				</div>
 			</Frame>
-		</nav>
+		</div>
 	);
 }
 
