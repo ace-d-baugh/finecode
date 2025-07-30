781f16ee63fad5da4231e5e7519aa0051a6b09f6
Ace Baugh <ace.d.baugh@gmail.com>
2025-07-29 21:45:08 -0400

    White bio

--- src/components/layout/ShortBio/ShortBio.tsx
+++ src/components/layout/ShortBio/ShortBio.tsx
@@ -6,7 +6,7 @@ import './ShortBio.css'
 function ShortBio() {
     return (
         <div className="ShortBio">
-            <h2>Who I Am</h2>
+            <h2>Who Am I?</h2>
             <p>Hi, I’m Ace, a full-stack web developer with a knack for building seamless React and Angular apps. With a Bachelor’s in Web Development and certifications in Mobile Apps, Internet Marketing, and UX/UI Design, I create user-focused solutions that shine —like my work for the Walt Disney Company. I’m obsessed with tech that makes an impact. To see what I do, check out my projects below or ...</p>
             <div className="shortBio-btn">
                 <Button text="Get To Know Me" link="/about" />
