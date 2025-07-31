9cf7e8e4a33faa1e8f5a838b2aa7d6d949b947a1
Ace Baugh <ace.d.baugh@gmail.com>
2025-07-30 21:56:46 -0400

    Hamburger menu working and looking better

--- src/components/layout/HamburgerMenu/HamburgerMenu.tsx
+++ src/components/layout/HamburgerMenu/HamburgerMenu.tsx
@@ -2,6 +2,7 @@
 
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
+import Frame from '../../common/Frame/Frame';
 import './HamburgerMenu.css';
 
 function HamburgerMenu() {
@@ -17,34 +18,38 @@ function HamburgerMenu() {
 				<span className="hamburger-icon"></span>
 			</button>
 			{isOpen && (
-				<ul className="hamburger-links">
-					<li>
-						<Link to="/about">
-							<i className="nf nf-oct-person_fill"> <span>About</span></i>
-						</Link>
-					</li>
-					<li>
-						<Link to="/portfolio">
-							<i className="nf nf-oct-briefcase"> <span>Portfolio</span></i>
-						</Link>
-					</li>
-					<li>
-						<Link to="/resume">
-							<i className="nf nf-oct-log"> <span>Resumé</span></i>
-						</Link>
-					</li>
-					<li>
-						<Link to="/blog">
-							<i className="nf nf-fa-book_journal_whills"> <span>Blog</span></i>
-						</Link>
-					</li>
-					<li>
-						<Link to="/contact">
-							<i className="nf nf-md-email_newsletter"> <span>Contact</span></i>
-						</Link>
-					</li>
-				</ul>
+				<div className="hamburger-links-container">
+					<ul className="hamburger-links">
+						<li>
+							<Link to="/about">
+								<i className="nf nf-oct-person_fill" /> <span>About</span>
+							</Link>
+						</li>
+						<li>
+							<Link to="/portfolio">
+								<i className="nf nf-oct-briefcase" /> <span>Portfolio</span>
+							</Link>
+						</li>
+						<li>
+							<Link to="/resume">
+								<i className="nf nf-oct-log" /> <span>Resumé</span>
+							</Link>
+						</li>
+						<li>
+							<Link to="/blog">
+								<i className="nf nf-fa-book_journal_whills" /> <span>Blog</span>
+							</Link>
+						</li>
+						<li>
+							<Link to="/contact">
+								<i className="nf nf-md-email_newsletter" /> <span>Contact</span>
+							</Link>
+						</li>
+					</ul>
+					<Frame className="hamburger-frame all"></Frame>
+				</div>
 			)}
+			
 		</div>
 	);
 }
