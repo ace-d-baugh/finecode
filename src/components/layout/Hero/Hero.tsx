2c1a516c70000c3a2651241c898c889c2f4243fa
Ace Baugh <ace.d.baugh@gmail.com>
2025-10-14 00:02:32 -0400

    adding searchlights to hero

--- src/components/layout/Hero/Hero.tsx
+++ src/components/layout/Hero/Hero.tsx
@@ -6,6 +6,12 @@ import './Hero.css'
 function Hero() {
     return (
         <div className="Hero">
+            <div className="searchlights no-animate">
+                <div className="searchlight sl-01"></div>
+                <div className="searchlight sl-02"></div>
+                <div className="searchlight sl-03"></div>
+                <div className="searchlight sl-04"></div>
+            </div>
             <div className="hero-content">
                 <h2>Elevate Your <wbr/>Digital&nbsp;Success</h2>
                 <p>Expert&nbsp;full-stack&nbsp;web&nbsp;solutions driving business&nbsp;growth&nbsp;and online&nbsp;success with Digital&nbsp;Elegance.</p>
