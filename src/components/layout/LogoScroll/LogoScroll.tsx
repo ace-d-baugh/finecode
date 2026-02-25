72a20098bb63183e9937a46e7a21fc08f82dae7a
Ace Baugh <ace.d.baugh@gmail.com>
2026-02-24 20:15:14 -0500

    logo images

--- src/components/layout/LogoScroll/LogoScroll.tsx
+++ src/components/layout/LogoScroll/LogoScroll.tsx
@@ -5,15 +5,15 @@ import './LogoScroll.css'
 function LogoScroll() {
     // Define logos array for reusability
     const logos = [
-        { src: "images/disney.png", alt: "The Walt Disney Company" },
-        { src: "images/carmike.png", alt: "Carmike Cinemas" },
-        { src: "images/rfinity.png", alt: "RFinity" },
-        { src: "images/schwans.png", alt: "Schwans" },
-        { src: "images/blockbuster.png", alt: "Blockbuster" },
-        { src: "images/misys.png", alt: "Misys" },
-        { src: "images/magnus.png", alt: "First Magnus" },
-        { src: "images/sears.png", alt: "Sears" },
-        { src: "images/kbtoys.png", alt: "KB Toys" },
+        { src: "/src/assets/images/logos/disney.png", alt: "The Walt Disney Company" },
+        { src: "/src/assets/images/logos/carmike.png", alt: "Carmike Cinemas" },
+        { src: "/src/assets/images/logos/rfinity.png", alt: "RFinity" },
+        { src: "/src/assets/images/logos/schwans.png", alt: "Schwans" },
+        { src: "/src/assets/images/logos/blockbuster.png", alt: "Blockbuster" },
+        { src: "/src/assets/images/logos/misys.png", alt: "Misys" },
+        { src: "/src/assets/images/logos/magnus.png", alt: "First Magnus" },
+        { src: "/src/assets/images/logos/sears.png", alt: "Sears" },
+        { src: "/src/assets/images/logos/kbtoys.png", alt: "KB Toys" },
     ];
 
     return (
