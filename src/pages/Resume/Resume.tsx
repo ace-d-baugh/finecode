79f872475936006b276fc80b1d834489da714477
ace-d-baugh <ace.d.baugh@gmail.com>
2026-02-25 00:46:18 +0000

    Improve Portfolio page styling - match landing page, better buttons, max-width 1200px

--- src/pages/Resume/Resume.tsx
+++ src/pages/Resume/Resume.tsx
@@ -1,5 +1,4 @@
 // pages/Resume/Resume.tsx
-
 import Education from "../../components/layout/Resume/Education/Education";
 import Experience from "../../components/layout/Resume/Experience/Experience";
 import Header from "../../components/layout/Resume/Header/Header";
@@ -10,19 +9,20 @@ import Work from "../../components/layout/Resume/Work/Work";
 import './Resume.css';
 
 function Resume() {
-    return (
-        <div className="Resume">
-            <div>
-                <Header />
-                <Summary />
-                <Skills />
-                <Work />
-                <Education />
-                <Experience />
-                <Recognition />
-            </div>
-        </div>
-    );
+  return (
+    <div className="Resume">
+      <h1>Resume</h1>
+      <div className="resume-content">
+        <Header />
+        <Summary />
+        <Skills />
+        <Work />
+        <Education />
+        <Experience />
+        <Recognition />
+      </div>
+    </div>
+  );
 }
 
-export default Resume
+export default Resume;
